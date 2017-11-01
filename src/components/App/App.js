81177d9931076e0b6700ffaae71b35319b03e1b2
coinyawong <30748399+coinyawong@users.noreply.github.com>
2017-11-01 19:17:16 +0900

    Update App.js
    fixed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,14 +9,14 @@ import { FlagsProvider } from 'flag';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import zh from 'react-intl/locale-data/zh';
 import ru from 'react-intl/locale-data/ru';
-import kr from 'react-intl/locale-data/kr';
+import ko from 'react-intl/locale-data/ko';
 
 import { COLORS, BREAKPOINTS, SPACE, FONT_SIZES, FLAGS } from 'config';
 import * as locales from 'locales';
 import Routes from '../Routes';
 import ScrollToTop from '../ScrollToTop';
 
-addLocaleData([...zh, ...ru, ...kr]);
+addLocaleData([...zh, ...ru, ...ko]);
 
 const theme = {
   colors: flatten(COLORS),
@@ -60,7 +60,7 @@ export default () => (
         <Switch>
           <Route path="/cn" render={props => <Root {...props} locale="zh" />} />
           <Route path="/ru" render={props => <Root {...props} locale="ru" />} />
-          <Route path="/kr" render={props => <Root {...props} locale="kr" />} />
+          <Route path="/kr" render={props => <Root {...props} locale="ko" />} />
           <Route path="/" render={props => <Root {...props} locale="en" />} />
         </Switch>
       </ScrollToTop>
